b4ff2610956810112b67175f39f6afed1ed568a1
Jonathan Tseng <me@tsengjonathan.com>
2022-06-06 18:24:52 -0400

    Add About section to Homepage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, _options) => {
+    config.module.rules.push({
+      test: /\.(mp4)$/,
+      use: {
+        loader: require.resolve('url-loader'),
+      },
+    });
+    return config;
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import MemberCard, { members } from '../components/MemberCard';
 import banner from '../public/banner.png';
 import cloudsLeft from '../public/clouds_left.png';
 import cloudsRight from '../public/clouds_right.png';
+import polaroid from '../public/polaroid.mp4';
 
 const Home: NextPage = () => {
   return (
@@ -43,10 +44,40 @@ const Home: NextPage = () => {
         />
       </section>
       <section className="bg-pink">
-        <div className="mx-auto max-w-screen-screen px-10 pt-12 text-xl font-bold text-white screen:px-0">
+        <div className="mx-auto max-w-screen-screen px-10 py-12 text-xl font-bold text-white screen:px-0">
           Welcome to the world and characters of GMCafé, most succulent and tantalising
           establishment in the metaverse. Come grab a cowffee, chill and unwind in a place where
           art, authenticity and community are still important.
+        </div>
+      </section>
+      <section className="bg-white">
+        <div className="mx-auto flex max-w-screen-screen flex-col px-10 py-12 md:flex-row screen:px-0">
+          <div className="flex flex-col justify-center">
+            <h1 className="text-left text-2xl font-semibold text-purple lg:text-4xl">About</h1>
+            <p className="text-light-purple mt-4 text-left font-semibold md:text-lg">
+              The Good Morning Café is founded on the strong and sturdy pillars of tender cuteness,
+              community connection and powerful cowffee. Each of the 333 ‘Genesis’ Highland Cows are
+              lovingly drawn into existence by artist, Ben Colefax. It all started as an obsession
+              with Parisian macarons, café vibes and the vision of a vast world of characters, all
+              converging in one pastel flavoured metaverse café.
+            </p>
+            <p className="text-light-purple mt-6 text-left font-semibold md:text-lg lg:mt-8">
+              It is said that each Highland Cow chooses its owner, using a series of vacant stares
+              and whispered moos that connects with the inner personality of the beholder. Your café
+              character NFT is your membership to the Herd - a group of similarly minded individuals
+              all mooving together.
+            </p>
+          </div>
+          <video
+            autoPlay
+            loop
+            playsInline
+            muted
+            onContextMenu={(e) => e.preventDefault()}
+            className="translate-y-12 md:w-[28rem]"
+          >
+            <source src={polaroid} />
+          </video>
         </div>
       </section>
       <section className="bg-pink py-10 px-10 screen:px-0">
