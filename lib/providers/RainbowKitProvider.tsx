34d355fa5c71a7b8422206069da34f4bb05be8cd
Jane <jzhen.wang@gmail.com>
2022-06-14 20:26:58 -0500

    update rainbowkit styling

--- lib/providers/RainbowKitProvider.tsx
+++ lib/providers/RainbowKitProvider.tsx
@@ -4,7 +4,10 @@ import '@rainbow-me/rainbowkit/styles.css';
 import {
   getDefaultWallets,
   RainbowKitProvider as _RainbowKitProvider,
+  lightTheme,
+  Theme
 } from '@rainbow-me/rainbowkit';
+import merge from 'lodash.merge';
 import { chain, createClient, configureChains, WagmiConfig } from 'wagmi';
 import { infuraProvider } from 'wagmi/providers/infura';
 import { publicProvider } from 'wagmi/providers/public';
@@ -29,9 +32,29 @@ export const RainbowKitProvider = ({ children }: RainbowKitProviderProps) => {
     provider,
   });
 
+  const cafeTheme = merge(lightTheme(), {
+    colors: {
+      accentColor: '#ff7dbd', // pink
+      accentColorForeground: '#fff',
+      closeButton: '#fff',
+      closeButtonBackground: '#ff7dbd', // pink
+      generalBorder: '#ffedf6', // light pink
+      modalBackdrop: 'rgba(255,255,255,0.3)',
+      modalText: '#ff7dbd', // pink
+      modalTextSecondary: '#8946ab', // purple,
+      menuItemBackground: '#ffedf6', // light pink
+    },
+    fonts: {
+      body: 'Quicksand',
+    },
+    shadows: {
+      dialog: '' // seems to remove the modal dialog shadow
+    }
+  } as Theme);
+
   return (
     <WagmiConfig client={wagmiClient}>
-      <_RainbowKitProvider chains={chains}>{children}</_RainbowKitProvider>
+      <_RainbowKitProvider chains={chains} theme={cafeTheme}>{children}</_RainbowKitProvider>
     </WagmiConfig>
   );
 };
