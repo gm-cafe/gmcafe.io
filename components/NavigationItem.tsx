5173718cd8b77f7f3d7197e66ada0757223dc7cd
Jonathan Tseng <me@tsengjonathan.com>
2022-06-18 00:09:41 -0400

    Revert "Support anchor smooth scrolling on iOS mobile"
    This reverts commit 8cec4145a7cb3572f61b96d8e241d7db03318bea.

--- components/NavigationItem.tsx
+++ components/NavigationItem.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames';
 import Link from 'next/link';
 import { ReactNode, MouseEvent, forwardRef, Ref } from 'react';
-import scrollIntoView from 'scroll-into-view-if-needed';
 
 type NavItemProps = {
   children: ReactNode | ReactNode[];
@@ -19,10 +18,7 @@ const NavigationItem = (
 
   const anchorOnClick = (e: MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
-
-    const node = href ? document.querySelector(href) : undefined;
-
-    href && node && scrollIntoView(node, { behavior: 'smooth', scrollMode: 'if-needed' });
+    href && document.querySelector(href)?.scrollIntoView({ behavior: 'smooth' });
     close && close();
   };
 
