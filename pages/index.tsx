7a16e1cd2a203912702419bcca7787d667293e4e
Jonathan Tseng <me@tsengjonathan.com>
2022-06-17 21:21:02 -0400

    Align moo peek with About section

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,8 +30,8 @@ const Home: NextPage = () => {
             <Image src={cloudsRight} layout="fill" alt="Clouds Right" objectFit="cover" />
           </div>
         </AnimatedPageSection>
-        <AnimatedPageSection className="bg-white px-10 sm:px-16 2xl:px-0">
-          <div className="relative mx-auto max-w-screen-2xl pt-12 pb-28">
+        <AnimatedPageSection className="bg-white px-10 pt-12 pb-28 sm:px-16 2xl:px-0">
+          <div className="mx-auto max-w-screen-2xl">
             <h2 className="font-gmcafe text-4xl uppercase text-purple md:text-5xl">
               Good Morning, Patron!
             </h2>
@@ -46,13 +46,13 @@ const Home: NextPage = () => {
                 <span className="font-gmcafe text-2xl text-purple md:text-3xl">Ben Colefax</span>
               </div>
             </div>
-            <div className="absolute bottom-0 left-0 w-48 md:w-64 translate-y-[29px] -translate-x-[20px] md:translate-x-0 md:translate-y-[39px]">
-              <Image src={mooPeek} layout="responsive" alt="Moo Peek" />
-            </div>
           </div>
         </AnimatedPageSection>
-        <AnimatedPageSection className="bg-pink px-10 py-12 sm:px-16 md:py-20 2xl:px-0">
-          <div className="mx-auto flex max-w-screen-2xl flex-col justify-center">
+        <AnimatedPageSection className="bg-pink px-10 sm:px-16 2xl:px-0">
+          <div className="relative mx-auto flex max-w-screen-2xl flex-col justify-center py-12 md:py-20">
+            <div className="absolute top-0 left-0 w-48 -translate-y-[7.5rem] -translate-x-[20px] md:w-64 md:-translate-y-40 md:translate-x-0">
+              <Image src={mooPeek} layout="responsive" alt="Moo Peek" />
+            </div>
             <h1 className="text-left font-gmcafe text-5xl uppercase text-white">About</h1>
             <p className="mt-4 text-left font-semibold text-white md:text-lg">
               The Good Morning Café is founded on the strong and sturdy pillars of tender cuteness,
@@ -62,8 +62,8 @@ const Home: NextPage = () => {
               converging in one pastel flavoured metaverse café.
             </p>
             <p className="mt-6 text-left font-semibold text-white md:text-lg lg:mt-8">
-              It is said that each Highland Cow chooses its owner, using a series of vacant stares and
-              whispered moos that connects with the inner personality of the beholder. Your café
+              It is said that each Highland Cow chooses its owner, using a series of vacant stares
+              and whispered moos that connects with the inner personality of the beholder. Your café
               character NFT is your membership to the Herd - a group of similarly minded individuals
               all mooving together.
             </p>
@@ -78,16 +78,17 @@ const Home: NextPage = () => {
             <h2 className="font-gmcafe text-5xl uppercase text-white">Genesis Collection</h2>
             <p className="mt-6 text-lg font-semibold text-white">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Adipiscing tristique risus nec feugiat in.
-              Amet aliquam id diam maecenas ultricies mi eget mauris pharetra. Non tellus orci ac
-              auctor augue mauris augue neque gravida. Posuere ac ut consequat semper. Leo integer
-              malesuada nunc vel risus commodo viverra maecenas. Ultrices sagittis orci a scelerisque
-              purus semper eget. Justo nec ultrices dui sapien eget mi. In eu mi bibendum neque
-              egestas congue quisque egestas diam. Arcu vitae elementum curabitur vitae nunc sed velit
-              dignissim. Aliquet lectus proin nibh nisl condimentum id venenatis a. Arcu non odio
-              euismod lacinia at. A diam maecenas sed enim ut sem viverra. Viverra orci sagittis eu
-              volutpat odio facilisis mauris sit. Dignissim convallis aenean et tortor at risus
-              viverra. Proin libero nunc consequat interdum varius sit.
+              incididunt ut labore et dolore magna aliqua. Adipiscing tristique risus nec feugiat
+              in. Amet aliquam id diam maecenas ultricies mi eget mauris pharetra. Non tellus orci
+              ac auctor augue mauris augue neque gravida. Posuere ac ut consequat semper. Leo
+              integer malesuada nunc vel risus commodo viverra maecenas. Ultrices sagittis orci a
+              scelerisque purus semper eget. Justo nec ultrices dui sapien eget mi. In eu mi
+              bibendum neque egestas congue quisque egestas diam. Arcu vitae elementum curabitur
+              vitae nunc sed velit dignissim. Aliquet lectus proin nibh nisl condimentum id
+              venenatis a. Arcu non odio euismod lacinia at. A diam maecenas sed enim ut sem
+              viverra. Viverra orci sagittis eu volutpat odio facilisis mauris sit. Dignissim
+              convallis aenean et tortor at risus viverra. Proin libero nunc consequat interdum
+              varius sit.
             </p>
           </div>
         </AnimatedPageSection>
