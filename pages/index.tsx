720556d2effccec355605139ca23af32499fc20c
Jane <jzhen.wang@gmail.com>
2022-07-05 23:03:21 -0500

    update FAQ

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,7 +71,7 @@ const Home: NextPage = () => {
                   <Image src={genesisHighland} layout="responsive" alt="FAQ" />
                 </div>
               </div>
-              <div className="squiggle squiggle-tall"/>
+              <div className="squiggle squiggle-tall" />
             </div>
             <div className="right-container col-span-1 bg-gray-100 py-12 px-24 md:col-span-2 md:py-20">
               <h2 className="font-gmcafe text-5xl uppercase text-purple">The Moos</h2>
@@ -120,7 +120,7 @@ const Home: NextPage = () => {
                   <Image src={phase2Brewing} layout="responsive" alt="FAQ" />
                 </div>
               </div>
-              <div className="squiggle"/>
+              <div className="squiggle" />
             </div>
             <div className="right-container col-span-1 bg-gray-100 py-12 px-24 md:col-span-2 md:py-20">
               <h2 className="font-gmcafe text-5xl uppercase text-purple">Oink, Ney, or Rawr?</h2>
@@ -151,7 +151,7 @@ const Home: NextPage = () => {
                   <Image src={lockEm} layout="responsive" alt="FAQ" />
                 </div>
               </div>
-              <div className="squiggle"/>    
+              <div className="squiggle" />
             </div>
             <div className="right-container col-span-1 bg-purple py-12 px-24 md:col-span-2 md:py-20">
               <h2 className="font-gmcafe text-5xl uppercase text-white">Moogration</h2>
@@ -178,27 +178,30 @@ const Home: NextPage = () => {
                   <Image src={faq} layout="responsive" alt="FAQ" />
                 </div>
               </div>
-              <div className="squiggle"/>    
+              <div className="squiggle" />
             </div>
             <div className="right-container col-span-1 bg-gray-100 py-12 px-24 md:col-span-2 md:py-20">
               <Disclosure question="How can I get a Genesis Moo?">
                 <p>
-                  You can currently purchase Moos on
+                  There are currently two ways now to obtain a moo and only one way to get an
+                  artist-issued moo. The first way is on the secondary market via
                   <a
                     className="ml-1.5 underline"
                     href="https://opensea.io/collection/goodmorningcafe"
                   >
                     OpenSea
                   </a>
-                  . Once we migrate the Moos to their own contract, you&apos;ll be able to purchase
-                  them across other marketplaces.
+                  . Once we migrate the Highland Cows to their own contract, you'll be able to
+                  purchase them across other reputable marketplaces.
                 </p>
-                <p>
-                  If you&apos;re interested in a custom Moo, you can open up a ticket in our
+                <p className="mt-4">
+                  The second, and only way to still get an original artist-issued Genesis moo, is
+                  via “Custom Moo”. Custom moos are designed by Ben using your ideas and inspiration
+                  and can be requested in the #support channel in the
                   <a className="ml-1.5 mr-0.5 underline" href="https://discord.gg/gmcafe">
                     Discord
                   </a>
-                  for information on pricing, etc.
+                  .
                 </p>
               </Disclosure>
               <Disclosure question="Will Moos be migrated to their own contract at some point?">
@@ -211,25 +214,26 @@ const Home: NextPage = () => {
                 </p>
               </Disclosure>
               <Disclosure question="What rights do I have to my Moo?">
-                <p>???</p>
+                <p>
+                  You are free to use your moo for personal use! For commercial use, there are some
+                  limitations.
+                </p>
               </Disclosure>
               <Disclosure question="Is there a roadmap?">
                 <p>
-                  Does soft and tender moo art count? Let’s just say that we are creating a “new
-                  menu” and it looks delicious. Stay close to
-                  <a className="ml-1.5 mr-1 underline" href="https://twitter.com/gmcafeNFT">
-                    Twitter
-                  </a>
-                  and
-                  <a className="ml-1.5 mr-1 underline" href="https://discord.gg/gmcafe">
-                    Discord
-                  </a>
-                  for announcements.
+                  Does soft and tender moo art count? Let’s just say that the GMCafé team and
+                  community is continually creating and shaping this world as we go. Ben has been
+                  building this character set for over 10 years, so roadmaps are less important when
+                  basically your whole career has been dedicated to it.
+                </p>
+                <p className="mt-4">
+                  Our only realistic promise is original and passionate artistic expression and
+                  being part of this new phase of popular art culture.
                 </p>
               </Disclosure>
               <Disclosure question="When will Phase 2 launch?">
                 <p>
-                  Soon. Keep an eye on our
+                  Phase 2 will soon be added to the menu and we hope it’s delicious. Stay close to
                   <a className="ml-1.5 mr-1 underline" href="https://twitter.com/gmcafeNFT">
                     Twitter
                   </a>
@@ -237,11 +241,14 @@ const Home: NextPage = () => {
                   <a className="ml-1.5 mr-1 underline" href="https://discord.gg/gmcafe">
                     Discord
                   </a>
-                  for updates.
+                  for announcements and updates.
                 </p>
               </Disclosure>
               <Disclosure question="Will holding a Moo give perks for Phase 2?">
-                <p>Yes! Mooooooooore details soon.</p>
+                <p>
+                  Emphatically, yes. We have been slowly but surely piecing together more Herd Perks
+                  for our floofy community using our new contract. MOOoore details soon.
+                </p>
               </Disclosure>
             </div>
           </div>
