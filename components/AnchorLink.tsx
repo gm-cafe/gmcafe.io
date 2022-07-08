a9bae4d48b3fcb98c929c4df6d90facc23a7832f
Jonathan Tseng <me@tsengjonathan.com>
2022-07-08 12:50:57 -0400

    Add new assets and hover effect on homepage buttons

--- components/AnchorLink.tsx
+++ components/AnchorLink.tsx
@@ -8,7 +8,7 @@ type AnchorLinkProps = {
 const AnchorLink = ({ href, children }: AnchorLinkProps) => {
   return (
     <a
-      className="text-shadow rounded-full bg-pink-light px-4 sm:px-5 pt-1 font-gmcafe text-2xl uppercase text-white lg:text-3xl"
+      className="text-shadow rounded-full bg-pink-light px-4 pt-1 font-gmcafe text-2xl uppercase text-white transition sm:px-5 lg:text-3xl betterhover:hover:scale-105 betterhover:hover:shadow"
       href={href}
     >
       {children}
