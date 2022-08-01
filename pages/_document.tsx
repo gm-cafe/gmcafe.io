748d13260b8f8da9dd99f8d079016588c2366446
Jonathan Tseng <me@tsengjonathan.com>
2022-08-01 19:21:15 -0400

    Revert "try removing document head completely"
    This reverts commit bd4527b10afdb5c71628fbded0e8a892bfad030f.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,18 @@
-import { Html, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Hepta+Slab:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+        <script defer data-domain="gmcafe.io" src="/js/script.js"></script>
+      </Head>
       <body className="cursor font-sans">
         <Main />
         <NextScript />
