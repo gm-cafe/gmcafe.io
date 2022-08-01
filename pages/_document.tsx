6861c21d2dcd3ffc4e4e8318d71c1ed5d87779da
Jonathan Tseng <me@tsengjonathan.com>
2022-08-01 19:21:30 -0400

    Revert "try removing document meta tags completely"
    This reverts commit 09f215f4df6f88bd329cb48f40b4ea9a637392e4.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,14 @@ export default function Document() {
 
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
 
+        <meta property="og:url" content="https://gmcafe.io" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Good Morning Café" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@gmcafeNFT" />
+        <meta name="twitter:title" content="Good Morning Café" />
+
         <script defer data-domain="gmcafe.io" src="/js/script.js"></script>
       </Head>
       <body className="cursor font-sans">
