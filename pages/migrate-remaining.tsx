1123564ae10a6747cde6ded26716db49ac8535cb
Jonathan Tseng <me@tsengjonathan.com>
2022-08-06 20:11:19 -0400

    add contact info

--- pages/migrate-remaining.tsx
+++ pages/migrate-remaining.tsx
@@ -2,12 +2,18 @@ import Head from 'next/head';
 import { useContractRead } from 'wagmi';
 import { gmooABI, gmooContract } from '../lib/util/addresses';
 import Image from 'next/image';
+import { GetStaticProps } from 'next';
+import Papa from 'papaparse';
+import { TwitterIcon, DiscordIcon } from '../components/Icons';
+import classNames from 'classnames';
 
 const lazyMintedMoos = [
   206, 223, 235, 238, 300, 308, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 333,
 ];
 
-const MigrateRemaining = () => {
+const sheetId = '1fWX_s0XrbBIJcC0jenzwvQtiiOnT8yjc2c57kWxyJUA';
+
+const MigrateRemaining = ({ contact }: { contact: (Contact | undefined)[] }) => {
   const { data } = useContractRead({
     addressOrName: gmooContract,
     contractInterface: gmooABI,
@@ -25,24 +31,103 @@ const MigrateRemaining = () => {
         <title>Remaining Moos</title>
       </Head>
       <div className="mx-auto flex max-w-screen-xl flex-wrap justify-center gap-4">
-        {unmigratedMoos.map((id) => (
-          <div className="relative w-40" key={id}>
-            <Image
-              className="rounded-xl"
-              src={`https://gmcafe.s3.us-east-2.amazonaws.com/gmoo/jpg/${id}.jpg`}
-              layout="responsive"
-              width={800}
-              height={800}
-              alt={`Highland Cow #${id}`}
-            />
-            <span className="absolute top-0 right-0 rounded-bl-lg rounded-tr-xl bg-white px-2 font-gmcafe text-lg text-purple 2xl:text-2xl">
-              {id}
-            </span>
-          </div>
-        ))}
+        {unmigratedMoos.map((id) => {
+          const twitter = contact[id]?.twitter;
+          const discord = contact[id]?.discordName;
+
+          return (
+            <div className="w-40" key={id}>
+              <div className="relative">
+                <Image
+                  className={classNames('rounded-t-xl', {
+                    'rounded-b-xl': !twitter && !discord,
+                  })}
+                  src={`https://gmcafe.s3.us-east-2.amazonaws.com/gmoo/jpg/${id}.jpg`}
+                  layout="responsive"
+                  width={800}
+                  height={800}
+                  alt={`Highland Cow #${id}`}
+                />
+                <span className="absolute top-0 right-0 rounded-bl-lg rounded-tr-xl bg-white px-2 font-gmcafe text-lg text-purple 2xl:text-2xl">
+                  {id}
+                </span>
+              </div>
+              {(twitter || discord) && (
+                <div className="flex flex-col gap-2 rounded-b-xl bg-white p-2">
+                  {twitter && (
+                    <a
+                      className="flex max-w-full items-center rounded-full bg-twitter px-2 py-1 transition betterhover:hover:scale-105"
+                      href={`https://twitter.com/${twitter}`}
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      <TwitterIcon className="h-3 w-3" fill="#ffffff" />
+                      <p className="ml-1.5 overflow-hidden whitespace-nowrap font-default text-xs text-white">
+                        @{twitter}
+                      </p>
+                    </a>
+                  )}
+                  {discord && (
+                    <span className="flex max-w-full items-center rounded-full bg-discord px-2 py-1">
+                      <DiscordIcon className="h-3 w-3" />
+                      <p className="ml-1.5 overflow-hidden whitespace-nowrap font-default text-xs text-white">
+                        {discord}
+                      </p>
+                    </span>
+                  )}
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
 
 export default MigrateRemaining;
+
+type Contact = {
+  twitter?: string;
+  discordName?: string;
+  discordId?: string;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  // one-indexed
+  const owners = await fetch(
+    `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=GMOO&range=AI:AI`
+  )
+    .then((response) => response.text())
+    .then((v) => Papa.parse<string[]>(v))
+    .then(({ data }) => data.map((row) => row[0]))
+    .catch((err) => console.log(err));
+
+  const contact: Record<string, Contact> = {};
+  // twitter, discord name, discord id, address
+  await fetch(
+    `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=Contact&range=B:E`
+  )
+    .then((response) => response.text())
+    .then((v) => Papa.parse<string[]>(v))
+    .then(({ data }) =>
+      data.forEach((row) => {
+        const address = row[3];
+        if (address && !contact[address]) {
+          contact[address] = {
+            twitter: row[0],
+            discordName: row[1],
+            discordId: row[2],
+          };
+        }
+      })
+    );
+
+  const mooContact = owners?.map((owner) => contact[owner] || null) || [];
+
+  return {
+    props: {
+      contact: mooContact,
+    },
+  };
+};
