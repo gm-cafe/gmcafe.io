ce0db047223fc1be51cd30d82660f9921423749a
Jonathan Tseng <me@tsengjonathan.com>
2022-08-16 18:11:41 -0400

    add arrows on moo profiles

--- pages/moo/[id].tsx
+++ pages/moo/[id].tsx
@@ -9,6 +9,8 @@ import { Moo } from '../../lib/util/types';
 import { useEnsAvatar, useEnsName } from 'wagmi';
 import mootag from '../../public/profile/moo_tag.png';
 import { Transition } from '@headlessui/react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
 
 const traitTypeStyle = 'font-gmcafe text-sm uppercase tracking-wider text-purple';
 const traitValueStyle = 'text-sm text-purple';
@@ -45,6 +47,9 @@ const Moo = ({ id }: Props) => {
   const isCustom =
     attributes.find(({ trait_type }) => trait_type === 'Delivery')?.value === 'Custom';
   const displayName = isCustom ? name.substring(name.indexOf(' - ') + 3) : name;
+
+  const isStart = id === 1;
+  const isEnd = id === 333;
 
   const separateAttributes = (
     <>
@@ -91,76 +96,98 @@ const Moo = ({ id }: Props) => {
         enterFrom="opacity-0 -translate-y-8"
         enterTo="opacity-100 translate-y-0"
       >
-        <div className="moo-tag-shadow relative pt-7 md:pt-12">
-          <svg
-            className="mx-auto -mb-[1px] w-[22.125rem] md:w-[44rem]"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1133.62 186.12"
-          >
-            <path
-              className="fill-white"
-              d="M 0 186.13 c 0 -46.37 37.86 -72.87 83.96 -83.96 L 525.49 4.5 c 27.1 -6 55.19 -6 82.29 -0.01 l 441.89 97.68 c 47.05 11.09 83.96 37.59 83.96 83.96 H 0 z"
-            />
-          </svg>
-          <div className="absolute top-0 left-1/2 w-52 -translate-x-7 md:w-80 md:-translate-x-10">
-            <Image src={mootag} layout="responsive" alt="Moo Tag" />
-          </div>
-        </div>
-        <div className="moo-verified-herd mx-4 flex w-[22.125rem] flex-col gap-4 rounded-b-3xl bg-white px-6 pt-6 pb-8 md:w-[44rem] md:flex-row md:px-10 md:pt-0 lg:mx-auto">
-          <div className="flex flex-col gap-4">
-            <div className="w-full md:w-64">
-              <Image
-                className="rounded-lg"
-                src={image}
-                width={800}
-                height={800}
-                layout="responsive"
-                alt={name}
-              />
+        <div className="flex items-center">
+          {!isStart && (
+            <div className="mt-20">
+              <Link href={`/moo/${id - 1}`}>
+                <ChevronLeftIcon className="h-14 w-14 cursor-pointer text-pink" />
+              </Link>
             </div>
-            <div className="hidden grid-cols-2 gap-x-2 gap-y-1 md:grid">{separateAttributes}</div>
-          </div>
-          <div className="flex-1">
-            <div className="flex items-center gap-4">
-              <h1 className="font-gmcafe text-4xl text-purple">{displayName}</h1>
-              <a
-                className="ml-auto"
-                href={`https://opensea.io/assets/ethereum/${gmooContract}/${id}`}
-                target="_blank"
-                rel="noreferrer"
+          )}
+          <div>
+            <div className="moo-tag-shadow relative pt-7 md:pt-12">
+              <svg
+                className="mx-auto -mb-[1px] w-[22.125rem] md:w-[44rem]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1133.62 186.12"
               >
-                <OpenSeaIcon className="h-6 w-6" fill="#2081E2" />
-              </a>
-            </div>
-            <div
-              style={{ borderColor: bgColor }}
-              className="mb-2 flex items-center gap-1 border-b-4 pt-1 pb-2"
-            >
-              {ensName && (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img
-                  className="h-4 w-4 rounded-full"
-                  src={ensAvatar || '/ens.png'}
-                  alt="ENS Avatar"
+                <path
+                  className="fill-white"
+                  d="M 0 186.13 c 0 -46.37 37.86 -72.87 83.96 -83.96 L 525.49 4.5 c 27.1 -6 55.19 -6 82.29 -0.01 l 441.89 97.68 c 47.05 11.09 83.96 37.59 83.96 83.96 H 0 z"
                 />
-              )}
-              <p className="truncate text-xs text-purple">{ensName || owner}</p>
+              </svg>
+              <div className="absolute top-0 left-1/2 w-52 -translate-x-7 md:w-80 md:-translate-x-10">
+                <Image src={mootag} layout="responsive" alt="Moo Tag" />
+              </div>
             </div>
-            <div className="grid grid-cols-2 gap-x-2 gap-y-1 md:hidden">{separateAttributes}</div>
-            <div className="grid grid-cols-2 gap-x-2 gap-y-1">
-              {attributes
-                .filter(
-                  ({ trait_type, value }) =>
-                    !customRenderTraits.includes(trait_type) && value !== 'None'
-                )
-                .map(({ value, trait_type }) => (
-                  <div key={`${trait_type}-${value}`}>
-                    <span className={traitTypeStyle}>{trait_type}</span>
-                    <p className={traitValueStyle}>{value}</p>
-                  </div>
-                ))}
+            <div className="moo-verified-herd mx-4 flex w-[22.125rem] flex-col gap-4 rounded-b-3xl bg-white px-6 pt-6 pb-8 md:w-[44rem] md:flex-row md:px-10 md:pt-0 lg:mx-auto">
+              <div className="flex flex-col gap-4">
+                <div className="w-full md:w-64">
+                  <Image
+                    className="rounded-lg"
+                    src={image}
+                    width={800}
+                    height={800}
+                    layout="responsive"
+                    alt={name}
+                  />
+                </div>
+                <div className="hidden grid-cols-2 gap-x-2 gap-y-1 md:grid">
+                  {separateAttributes}
+                </div>
+              </div>
+              <div className="flex-1">
+                <div className="flex items-center gap-4">
+                  <h1 className="font-gmcafe text-4xl text-purple">{displayName}</h1>
+                  <a
+                    className="ml-auto"
+                    href={`https://opensea.io/assets/ethereum/${gmooContract}/${id}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <OpenSeaIcon className="h-6 w-6" fill="#2081E2" />
+                  </a>
+                </div>
+                <div
+                  style={{ borderColor: bgColor }}
+                  className="mb-2 flex items-center gap-1 border-b-4 pt-1 pb-2"
+                >
+                  {ensName && (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                      className="h-4 w-4 rounded-full"
+                      src={ensAvatar || '/ens.png'}
+                      alt="ENS Avatar"
+                    />
+                  )}
+                  <p className="truncate text-xs text-purple">{ensName || owner}</p>
+                </div>
+                <div className="grid grid-cols-2 gap-x-2 gap-y-1 md:hidden">
+                  {separateAttributes}
+                </div>
+                <div className="grid grid-cols-2 gap-x-2 gap-y-1">
+                  {attributes
+                    .filter(
+                      ({ trait_type, value }) =>
+                        !customRenderTraits.includes(trait_type) && value !== 'None'
+                    )
+                    .map(({ value, trait_type }) => (
+                      <div key={`${trait_type}-${value}`}>
+                        <span className={traitTypeStyle}>{trait_type}</span>
+                        <p className={traitValueStyle}>{value}</p>
+                      </div>
+                    ))}
+                </div>
+              </div>
             </div>
           </div>
+          {!isEnd && (
+            <div className="mt-20">
+              <Link href={`/moo/${id + 1}`}>
+                <ChevronRightIcon className="h-14 w-14 cursor-pointer text-pink" />
+              </Link>
+            </div>
+          )}
         </div>
       </Transition>
     </div>
