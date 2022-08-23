130f4b0c3cf73ab644f5d59de63518280fe94a68
Jonathan Tseng <me@tsengjonathan.com>
2022-08-22 22:59:08 -0400

    color tweaks

--- components/leaderboard/Herd.tsx
+++ components/leaderboard/Herd.tsx
@@ -1,3 +1,4 @@
+import { ExternalLinkIcon } from '@heroicons/react/solid';
 import Image from 'next/image';
 import { HerdInfoByOwner } from '../../lib/util/types';
 
@@ -5,9 +6,14 @@ type Props = HerdInfoByOwner;
 
 const Herd = ({ owner, herd }: Props) => {
   return (
-    <div className="flex flex-col gap-4 rounded-lg bg-white p-4 md:flex-row md:flex-wrap md:items-center">
-      <p className="shrink-0 truncate text-xs text-purple md:text-sm">{owner}</p>
-      <div className="flex flex-wrap justify-evenly gap-2 md:ml-auto md:justify-end">
+    <div className="flex flex-col rounded-lg bg-white">
+      <div className="flex items-center gap-2 rounded-t-lg bg-pink-light px-4 py-2">
+        <p className="truncate text-xs text-purple md:text-sm">{owner}</p>
+        <a className="ml-auto" href="https://etherscan.io">
+          <ExternalLinkIcon className="h-6 w-6 text-purple" />
+        </a>
+      </div>
+      <div className="flex flex-wrap gap-2 p-4">
         {herd.map(({ moo }) => (
           <div className="h-12 w-12 md:h-20 md:w-20" key={`${owner}-${moo}`}>
             <Image
--- components/leaderboard/HerdGroup.tsx
+++ components/leaderboard/HerdGroup.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const HerdGroup = ({ count, herdByOwner }: Props) => {
   return (
-    <div className="flex w-full flex-col gap-2 rounded-xl border-2 border-purple bg-purple-light p-4">
+    <div className="flex w-full flex-col gap-2 rounded-xl bg-pink p-4">
       <h2 className="font-gmcafe text-2xl text-purple">{count}</h2>
       <div className="flex flex-col gap-2">
         {herdByOwner.map(({ owner, herd }) => (
