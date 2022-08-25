50536ffd42ee6993d633e85b648c1f00660733d3
Jonathan Tseng <me@tsengjonathan.com>
2022-08-25 01:37:24 -0400

    Add Associate Page (#9)
    * create associate page
    
    * add a meta description tag
    
    * update styling, signature message

---
+++ pages/associate.tsx
@@ -0,0 +1,94 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import classNames from 'classnames';
+import { NextPage } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useAccount, useSignMessage } from 'wagmi';
+import { LoadingIcon } from '../components/Icons';
+import { Discord } from '../components/StyledLinks';
+
+const description = 'Associate your wallet with your Discord username using the M007 bot.';
+
+const Associate: NextPage = () => {
+  const [apiSuccess, setApiSuccess] = useState(false);
+  const [apiLoading, setApiLoading] = useState(false);
+
+  const { address } = useAccount();
+
+  const { query } = useRouter();
+  const { nonce } = query;
+
+  const { signMessage } = useSignMessage({
+    message: `Moo! Please sign to link your wallet address to your Discord account.\n\nOwner: ${address}\nNonce: ${nonce}`,
+    onMutate: () => setApiLoading(true),
+    onSettled: () => setApiLoading(false),
+    onSuccess: (data) => {
+      const body = {
+        owner: address,
+        sig: data,
+      };
+      fetch('https://api.gmcafe.io/bot-api/associate', {
+        method: 'POST',
+        body: JSON.stringify(body),
+      })
+        .then(() => setApiSuccess(true))
+        .catch(console.error);
+    },
+  });
+
+  const nonceNotFound = (
+    <div className="m-4 flex max-w-screen-sm flex-col gap-2 rounded-xl bg-white p-6 md:mx-auto">
+      <h1 className="font-gmcafe text-4xl text-purple">Invalid Discord URL</h1>
+      <p className="text-sm text-purple">
+        Please try again using the link generated from the{' '}
+        <span className="font-semibold">/associate</span> Discord command, or open a Support ticket
+        in the <Discord /> Server.
+      </p>
+    </div>
+  );
+
+  const associateButton = (
+    <button
+      className={classNames(
+        'relative rounded-2xl bg-pink px-4 py-2 font-gmcafe text-2xl text-white',
+        { 'transition-transform hover:scale-105': !apiLoading }
+      )}
+      onClick={() => signMessage()}
+      disabled={apiLoading}
+    >
+      <span className={classNames('uppercase', { 'opacity-0': apiLoading })}>Associate</span>
+      <span className="absolute top-0 right-0 flex h-full w-full items-center justify-center">
+        {apiLoading && <LoadingIcon />}
+      </span>
+    </button>
+  );
+
+  const successMessage = (
+    <div className="m-4 flex max-w-screen-sm flex-col gap-2 rounded-xl bg-white p-6 md:mx-auto">
+      <h1 className="font-gmcafe text-4xl text-purple">Success!</h1>
+      <p className="text-sm text-purple">
+        Your wallet is successfully associated with your Discord username and you can now close this
+        window.
+      </p>
+    </div>
+  );
+
+  return (
+    <div className="flex min-h-screen bg-pink-background">
+      <Head>
+        <title>Associate</title>
+        <meta name="description" content={description} key="desc" />
+        <meta property="og:description" content={description} key="ogDesc" />
+      </Head>
+      <div className="flex flex-1 flex-col items-center justify-center gap-4">
+        {!nonce && nonceNotFound}
+        {!apiSuccess && nonce && <ConnectButton />}
+        {!apiSuccess && nonce && address && associateButton}
+        {apiSuccess && successMessage}
+      </div>
+    </div>
+  );
+};
+
+export default Associate;
