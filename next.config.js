cf694ff2742a6d84a59746a9297347666fca7d40
Jonathan Tseng <me@tsengjonathan.com>
2023-02-20 22:10:03 -0500

    chore: update example metadata

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const moduleExports = {
     return config;
   },
   images: {
-    domains: ['lh3.googleusercontent.com', 'gmcafe.s3.us-east-2.amazonaws.com'],
+    domains: ['lh3.googleusercontent.com', 'gmcafe.s3.us-east-2.amazonaws.com', 'cdn.discordapp.com'],
   },
   async rewrites() {
     return [
