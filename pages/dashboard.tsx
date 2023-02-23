e243996f6b65514df7a0afe41e6d6cb268f38547
Jonathan Tseng <me@tsengjonathan.com>
2023-02-22 23:02:57 -0500

    fix: dashboard moos overflow padding

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -63,7 +63,7 @@ const Dashboard = () => {
   }
 
   return (
-    <div className="flex min-h-screen items-center bg-pink-background">
+    <div className="flex min-h-screen items-center bg-pink-background pt-40 pb-12">
       <div className="mx-auto flex w-full max-w-screen-sm flex-col items-center justify-center">
         <nav
           className={classNames(
