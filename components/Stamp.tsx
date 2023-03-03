2e6d9938e6e2e4d9a4b00929707dd6e8f8a2f9b1
Jonathan Tseng <me@tsengjonathan.com>
2023-03-02 19:26:08 -0500

    feat: add easter egg for stamps

---
+++ components/Stamp.tsx
@@ -0,0 +1,69 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import { AES, enc } from 'crypto-js';
+
+import stamp from '../public/stamp.png';
+
+const passphrase = 'benjaneloopraffy';
+
+const Stamp = () => {
+  const [clicked, setClicked] = useState(false);
+  const [discordName, setDiscordName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const encryptValue = AES.encrypt(`grawr/${discordName}/${Date.now()}`, passphrase);
+  const decryptValue = AES.decrypt(encryptValue, passphrase);
+
+  console.log(discordName, encryptValue.toString(), decryptValue.toString(enc.Utf8));
+
+  return (
+    <div className="flex w-full justify-end md:col-span-2">
+      {!clicked && (
+        <div
+          onClick={() => setClicked(true)}
+          className="hover:scale-120 absolute -right-8 bottom-0 w-28 cursor-pointer transition-transform hover:scale-125 md:w-36"
+        >
+          <Image src={stamp} alt="Stamp" />
+        </div>
+      )}
+      {clicked && (
+        <div>
+          <p className="font-gmcafe text-lg text-purple">You Found Me!</p>
+          {!submitted && (
+            <div className="flex flex-col">
+              <p className="font-gmcafe text-lg text-purple">Paste your Discord Username Here:</p>
+              <input
+                className="mb-4 rounded border-2 border-purple px-1 py-1 text-purple placeholder:text-purple-light focus:outline-none"
+                placeholder="BenColefax#0001"
+                value={discordName}
+                onChange={(e) => setDiscordName(e.target.value)}
+              />
+              <div>
+                <button
+                  className="rounded-lg bg-purple px-2 font-gmcafe text-lg text-white"
+                  onClick={() => setSubmitted(true)}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          )}
+          {submitted && (
+            <div>
+              <p className="text-purple">Here&apos;s your secret code:</p>
+              <p className="text-gmcafe break-all font-gmcafe text-lg text-purple underline">
+                {encryptValue.toString()}
+              </p>
+              <p className="text-purple">
+                Tag <span className="font-gmcafe">@Loop</span> in #social-chat with your secret
+                message!
+              </p>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Stamp;
--- components/traits/Viewer.tsx
+++ components/traits/Viewer.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { gmooContract } from '../../lib/util/addresses';
 import { Moo } from '../../lib/util/types';
 import { OpenSeaIcon } from '../Icons';
+import Stamp from '../Stamp';
 
 type Props = {
   metadata?: Moo;
@@ -26,7 +27,7 @@ const Viewer = ({ metadata, onClose }: Props) => {
 
       <div className="fixed inset-0 overflow-y-auto">
         <div className="flex min-h-full items-center justify-center p-4">
-          <Dialog.Panel className="relative mx-auto grid grid-cols-1 gap-8 rounded-3xl bg-white p-8 md:grid-cols-2 md:gap-16 md:p-16">
+          <Dialog.Panel className="relative mx-auto grid grid-cols-1 gap-8 overflow-hidden rounded-3xl bg-white p-8 md:grid-cols-2 md:gap-16 md:p-16">
             <button className="absolute top-3 right-3 focus:outline-none" onClick={onClose}>
               <XIcon className="h-5 w-5 text-[#A3A19A]" />
             </button>
@@ -66,6 +67,7 @@ const Viewer = ({ metadata, onClose }: Props) => {
                 </a>
               </div>
             </div>
+            {id === 292 && <Stamp />}
           </Dialog.Panel>
         </div>
       </div>
