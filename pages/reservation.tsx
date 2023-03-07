ec0a96eda8a8929d692ec4c8a711e0e76a979c76
Jonathan Tseng <me@tsengjonathan.com>
2023-03-06 22:35:46 -0500

    chore(reservation): add more edge cases

--- pages/reservation.tsx
+++ pages/reservation.tsx
@@ -10,8 +10,9 @@ import Image from 'next/image';
 import { loadFull } from 'tsparticles';
 import { Engine } from 'tsparticles-engine';
 import Particles from 'react-tsparticles';
+import { Discord } from '../components/StyledLinks';
 
-type State = 'typing' | 'confirmed' | 'unconfirmed';
+type State = 'typing' | 'confirmed' | 'unconfirmed' | 'not in discord';
 
 const Reservation: NextPage = () => {
   const [input, setInput] = useState('');
@@ -20,6 +21,12 @@ const Reservation: NextPage = () => {
   const [card, setCard] = useState('');
   const [downloadLoading, setDownloadLoading] = useState(false);
   const [cacheCardLoading, setCacheCardLoading] = useState(false);
+  const [moos, setMoos] = useState(0);
+  const [chonks, setChonks] = useState(0);
+  const [fnds, setFnds] = useState(0);
+
+  const mintCount = chonks === 1 ? 1 : chonks === 5 ? 2 : chonks === 10 ? 3 : 1;
+  const airdropCount = moos + fnds;
 
   const isValid = utils.isAddress(input) || input.endsWith('.eth');
 
@@ -48,8 +55,11 @@ const Reservation: NextPage = () => {
         if (Object.keys(json).length === 0) {
           setState('unconfirmed');
         } else {
-          setState('confirmed');
-          setCard(json.card);
+          setState(json.not_in_discord ? 'not in discord' : 'confirmed');
+          json.card && setCard(json.card);
+          json.moos && setMoos(json.moos.length);
+          json.chonker && setChonks(json.chonker);
+          json.fnds && setFnds(json.fnds.length);
         }
       })
       .catch(toastError)
@@ -115,9 +125,9 @@ const Reservation: NextPage = () => {
   };
 
   return (
-    <div className="flex min-h-screen bg-pink-background">
+    <div className="mt-22 flex min-h-screen bg-pink-background">
       <div className="mx-auto flex max-w-screen-sm flex-1 flex-col items-center justify-center gap-6 p-2">
-        {state !== 'confirmed' && (
+        {!['confirmed', 'not in discord'].includes(state) && (
           <div className="w-full">
             <span className="mx-2 font-gmcafe text-lg text-purple">Address/ENS name</span>
             <input
@@ -137,42 +147,73 @@ const Reservation: NextPage = () => {
           </div>
         )}
         {card && (
-          <div>
-            <div className="rounded-[1.2rem] bg-white p-3 md:rounded-[2rem]">
-              <Image width={1391} height={794} src={imgUrl} alt="Card" unoptimized />
-            </div>
-            <p className="mt-1 text-center font-gmcafe text-lg text-purple">
-              Your reservation is confirmed. Come back on mint day to adopt your Keekusaur!
-            </p>
-            <div className="mt-3 flex justify-center gap-4">
-              <button
-                className="flex items-center gap-1 rounded-lg bg-purple py-2 pl-3 pr-4 transition-colors hover:bg-opacity-80"
-                onClick={onDownload}
-              >
-                {!downloadLoading && (
-                  <>
-                    <DownloadIcon className="h-6 w-6 text-white" />
-                    <span className="font-gmcafe text-lg text-white">Download</span>
-                  </>
-                )}
-                {downloadLoading && <LoadingIcon />}
-              </button>
-              <button
-                className="flex items-center gap-2 rounded-lg bg-purple py-2 px-4 transition-colors hover:bg-opacity-80"
-                onClick={onTwitterShare}
-              >
-                {!cacheCardLoading && (
-                  <>
-                    <TwitterIcon className="h-6 w-6" fill="#ffffff" />
-                    <span className="font-gmcafe text-lg text-white">Share</span>
-                  </>
-                )}
-                {cacheCardLoading && <LoadingIcon />}
-              </button>
-            </div>
+          <div className="rounded-[1.2rem] bg-white p-3 md:rounded-[2rem]">
+            <Image width={1391} height={794} src={imgUrl} alt="Card" unoptimized />
           </div>
         )}
-        {state !== 'confirmed' && (
+        {state === 'confirmed' && card && (
+          <p className="mt-1 text-center font-gmcafe text-lg text-purple">
+            Your reservation is confirmed. Come back on mint day to adopt your Keekusaur!
+            {mintCount > 1 && (
+              <span>
+                {' '}
+                You will be able to mint{' '}
+                <span className="rounded-lg bg-white px-1.5 text-2xl">{mintCount}</span> Keekusaurs
+                on mint day!
+              </span>
+            )}
+          </p>
+        )}
+        {airdropCount > 0 && (
+          <p className="text-center font-gmcafe text-lg text-purple">
+            Congrats you{card ? ' also' : ''} have a{' '}
+            {moos > 0 && fnds > 0
+              ? 'Moo and Foundation Piece'
+              : fnds > 0
+              ? 'Foundation Piece'
+              : 'Moo'}
+            !{' '}
+            <span className={classNames({ 'rounded-lg bg-white px-1.5 text-2xl': moos > 1 })}>
+              {moos === 1 ? 'A' : moos}
+            </span>{' '}
+            Keekusaur
+            {moos === 1 ? '' : 's'} will be airdropped to your wallet
+          </p>
+        )}
+        {state === 'not in discord' && (
+          <p className="text-center font-gmcafe text-lg text-purple">
+            You must be in our <Discord /> to keep your reservation.
+          </p>
+        )}
+        {card && (
+          <div className="mt-3 flex justify-center gap-4">
+            <button
+              className="flex items-center gap-1 rounded-lg bg-purple py-2 pl-3 pr-4 transition-colors hover:bg-opacity-80"
+              onClick={onDownload}
+            >
+              {!downloadLoading && (
+                <>
+                  <DownloadIcon className="h-6 w-6 text-white" />
+                  <span className="font-gmcafe text-lg text-white">Download</span>
+                </>
+              )}
+              {downloadLoading && <LoadingIcon />}
+            </button>
+            <button
+              className="flex items-center gap-2 rounded-lg bg-purple py-2 px-4 transition-colors hover:bg-opacity-80"
+              onClick={onTwitterShare}
+            >
+              {!cacheCardLoading && (
+                <>
+                  <TwitterIcon className="h-6 w-6" fill="#ffffff" />
+                  <span className="font-gmcafe text-lg text-white">Share</span>
+                </>
+              )}
+              {cacheCardLoading && <LoadingIcon />}
+            </button>
+          </div>
+        )}
+        {!['confirmed', 'not in discord'].includes(state) && (
           <button
             className={classNames(
               'rounded-lg bg-white px-3 py-1 font-gmcafe text-xl text-purple transition-opacity',
