e071eee389b7e9735c61c5760cb573087bdf78e4
Jonathan Tseng <me@tsengjonathan.com>
2023-03-13 18:56:38 -0400

    chore: card tweaks

--- components/mint/Card.tsx
+++ components/mint/Card.tsx
@@ -12,11 +12,13 @@ const Card = ({ current, index, count, next }: Props) => {
   return (
     <div
       className={classNames(
-        'absolute h-full w-full cursor-pointer rounded-xl border-4 border-white bg-white shadow-lg-purple transition-all duration-500 hover:border-purple',
+        'absolute h-full w-full cursor-pointer rounded-xl bg-white shadow-lg-purple transition-all duration-200 hover:!scale-110',
         { 'opacity-0': current > index }
       )}
       style={{
-        transform: `rotate(${current >= index ? 0 : index * 5}deg)`,
+        transform: `rotate(${
+          current >= index ? 0 : index * 5 * (Math.random() > 0.5 ? -1 : 1)
+        }deg)`,
         zIndex: current > index ? 0 : count - index,
       }}
       onClick={next}
