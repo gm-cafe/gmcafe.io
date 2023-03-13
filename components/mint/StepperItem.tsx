4a1829d88047309a4288c1813df531fa72daa3c6
Jonathan Tseng <me@tsengjonathan.com>
2023-03-12 22:11:15 -0400

    chore: mobile tweaks

--- components/mint/StepperItem.tsx
+++ components/mint/StepperItem.tsx
@@ -7,9 +7,9 @@ type Props = {
 };
 
 const StepperItem = ({ current, index, title }: Props) => {
-  const itemClassName = 'flex-1 flex flex-col w-32 text-center stepper-item';
+  const itemClassName = 'flex-1 flex flex-col w-16 md:w-32 text-center stepper-item';
   const titleClassName =
-    'font-gmcafe text-purple text-lg bg-white rounded-full relative stepper-title';
+    'font-gmcafe text-purple md:text-lg bg-white rounded-full relative stepper-title mx-auto px-3';
 
   return (
     <li
